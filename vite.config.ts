f0d6c3acc3f50d05ca34a68fb8f5f3b03073a646
Stephen Maina <stevenmaina17@gmail.com>
2025-10-19 08:56:29 +0000

    server updates

--- vite.config.ts
+++ vite.config.ts
@@ -8,22 +8,17 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
             refresh: true,
+            buildDirectory: 'build',
         }),
         tailwindcss(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
     ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './resources/js'),
         },
     },
+    base: '/buyalot/build/',
 });
+
