e7f32999ea19ca241d2163e24d2459334b7aa1b3
smaina <smaina@sgasecurity.com>
2025-10-20 22:11:30 +0300

    sellers updates, payaments updates

---
+++ app/Models/Seller/SellerUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Seller;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SellerUser extends Model
+{
+    protected $guarded = [];
+    protected $table = 'seller_user';
+}
---
+++ database/migrations/2025_10_20_220019_update_seller_user_foreign_key_to_seller_applications.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('seller_user', function (Blueprint $table) {
+            $table->dropForeign(['seller_id']);
+            $table->foreign('seller_id')
+                ->references('id')
+                ->on('seller_applications')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('seller_user', function (Blueprint $table) {
+            $table->dropForeign(['seller_id']);
+            $table->foreign('seller_id')
+                ->references('id')
+                ->on('sellers')
+                ->onDelete('cascade');
+        });
+    }
+};
