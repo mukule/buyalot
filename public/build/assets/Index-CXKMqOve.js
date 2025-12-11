f9ad088e94fe4f57ddc6a1075eb0da9a87fcfeaa
mukule <nelsonmasibo6@gmail.com>
2025-12-11 12:21:43 +0300

    Add production build

---
+++ public/build/assets/Index-CXKMqOve.js
@@ -0,0 +1,1 @@
+import{d as e}from"./app-Bha5sprQ.js";const o=e({__name:"Index",setup(n){return(t,r)=>null}});export{o as default};
---
+++ public/build/assets/customers.loyalty-points.index-NlT9p0BP.js
@@ -0,0 +1,1 @@
+import{d as e}from"./app-Bha5sprQ.js";const s=e({__name:"customers.loyalty-points.index",setup(t){return(n,o)=>null}});export{s as default};
